b97916a1e8ab7588ee0c1eac322013317b45a020
Grigoriy <grisha101202@yandex,ru>
2021-03-18 18:01:22 +0300

    ended logical

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -3,22 +3,54 @@ package com.example.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int second;
+    private boolean isRunning;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textViewTimer);
+        runTimer();
     }
 
     public void OnClickStartTimer(View view) {
+        isRunning = true;
     }
 
     public void OnClickPauseTimer(View view) {
+        isRunning = false;
     }
 
     public void OnClickSkipTimer(View view) {
+        isRunning = false;
+        second = 0;
+    }
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = second/3600;
+                int min = second%3600/60;
+                int sec = second %60;
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, min, sec);
+                textView.setText(time);
+
+                if(isRunning) second++;
+                handler.postDelayed(this,1000);
+            }
+        });
+
     }
 }
