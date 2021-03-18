3d030a1b07095b7baed6df0543c283ce74d9f37a
Grigoriy <grisha101202@yandex,ru>
2021-03-18 17:46:01 +0300

    change maket

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnClickStartTimer(View view) {
+    }
+
+    public void OnClickPauseTimer(View view) {
+    }
+
+    public void OnClickSkipTimer(View view) {
     }
 }
